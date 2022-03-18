eda4b4c29b5f486f7ca97e15d26a2f1b770fa8aa
McKenna <Mckennaburch19@gmail.com>
2022-03-18 10:03:39 -0500

    added pre defined data into seeds file

---
+++ db/seed.sql
@@ -0,0 +1,39 @@
+USE employees_DB
+
+INSERT INTO department (name)
+VALUES ("Sales");
+INSERT INTO department (name)
+VALUES ("Engineering");
+INSERT INTO department (name)
+VALUES ("Finance");
+INSERT INTO department (name)
+VALUES ("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 5);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jane", "Doe", 1, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mickie", "Mouse", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chan", 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Kevin", "Tupik", 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sarah", "Hurst", 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Gary", "Leer", 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Tom", "Allen", 4, 7);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Jason", "Hooper", 1, 2);
