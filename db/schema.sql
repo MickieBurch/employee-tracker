7617ed5f862d9c960b0aa3afdd06d16a01f25195
McKenna <Mckennaburch19@gmail.com>
2022-03-18 09:57:52 -0500

    added the employee table and also the USE call at the top

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS employees_DB;
 CREATE DATABASE employees_DB
 
+USE employees_DB
+
 CREATE TABLE department(
  id INTEGER NOT NULL AUTO_INCREMENT,
  name: VARCHAR(30) NOT NULL,
@@ -12,6 +14,15 @@ id INT NOT NULL AUTO_INCREMENT,
 name: VARCHAR(30) NOT NULL, 
 salary DECIMAL(10,3)
 department_id INT NULL,
+PRIMARY KEY (id)
 );
 
 
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    First_name: VARCHAR(30) NOT NULL, 
+    last_name: VARCHAR(30) NOT NULL, 
+    role_id INT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id)
+);
