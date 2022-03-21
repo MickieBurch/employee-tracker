28dd20e5ffc4e3332fd311b025fe11028d5cca39
McKenna <Mckennaburch19@gmail.com>
2022-03-20 20:04:54 -0500

    added the ability to connect to mysql via the server.js file

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
+//dependencies required
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
+//const sql = require("./sql");
+
+//mysql connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+  
+    port: 3306,
+
+
+    user: 'root',
+
+
+    password: 'Lillymoonheart13!',
+    database: 'employees_DB'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(``)
+    // runs the app
+    firstPrompt();
+});
+
